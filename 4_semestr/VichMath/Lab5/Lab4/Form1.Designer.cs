59c9813c96589b7b7cc25e9d864ecb10d32932b2
OranGeNaL <pshenichniy2514@gmail.com>
2020-05-13 13:02:23 +1000

    Сдал 4 лабу

---
+++ 4_semestr/VichMath/Lab5/Lab4/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.importButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.shouDotsButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.countButton = new System.Windows.Forms.Button();
+            this.showCouples = new System.Windows.Forms.Button();
+            this.rectangleMethodButton = new System.Windows.Forms.Button();
+            this.drawFuncButton = new System.Windows.Forms.Button();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxN = new System.Windows.Forms.TextBox();
+            this.alable = new System.Windows.Forms.Label();
+            this.blable = new System.Windows.Forms.Label();
+            this.Nlabel = new System.Windows.Forms.Label();
+            this.integralButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // importButton
+            // 
+            this.importButton.Enabled = false;
+            this.importButton.Font = new System.Drawing.Font("Rubik", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.importButton.Location = new System.Drawing.Point(518, 355);
+            this.importButton.Name = "importButton";
+            this.importButton.Size = new System.Drawing.Size(167, 37);
+            this.importButton.TabIndex = 0;
+            this.importButton.Text = "Импорт";
+            this.importButton.UseVisualStyleBackColor = true;
+            this.importButton.Click += new System.EventHandler(this.importButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(500, 500);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Font = new System.Drawing.Font("Rubik", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.refreshButton.Location = new System.Drawing.Point(662, 480);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(162, 31);
+            this.refreshButton.TabIndex = 3;
+            this.refreshButton.Text = "Обновить форму";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Rubik", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.clearButton.Location = new System.Drawing.Point(662, 426);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(162, 48);
+            this.clearButton.TabIndex = 5;
+            this.clearButton.Text = "Очистить всё";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // shouDotsButton
+            // 
+            this.shouDotsButton.Font = new System.Drawing.Font("Rubik", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.shouDotsButton.Location = new System.Drawing.Point(543, 426);
+            this.shouDotsButton.Name = "shouDotsButton";
+            this.shouDotsButton.Size = new System.Drawing.Size(113, 85);
+            this.shouDotsButton.TabIndex = 6;
+            this.shouDotsButton.Text = "Показать координаты точек";
+            this.shouDotsButton.UseVisualStyleBackColor = true;
+            this.shouDotsButton.Click += new System.EventHandler(this.shouDotsButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rubik", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(543, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Импортируйте пары значений";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(518, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(281, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // countButton
+            // 
+            this.countButton.Font = new System.Drawing.Font("Rubik", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.countButton.Location = new System.Drawing.Point(518, 111);
+            this.countButton.Name = "countButton";
+            this.countButton.Size = new System.Drawing.Size(167, 57);
+            this.countButton.TabIndex = 11;
+            this.countButton.Text = "Посчитать в заданной точке";
+            this.countButton.UseVisualStyleBackColor = true;
+            this.countButton.Click += new System.EventHandler(this.countButton_Click);
+            // 
+            // showCouples
+            // 
+            this.showCouples.Font = new System.Drawing.Font("Rubik", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.showCouples.Location = new System.Drawing.Point(518, 312);
+            this.showCouples.Name = "showCouples";
+            this.showCouples.Size = new System.Drawing.Size(167, 37);
+            this.showCouples.TabIndex = 1;
+            this.showCouples.Text = "Вывести пары";
+            this.showCouples.UseVisualStyleBackColor = true;
+            this.showCouples.Click += new System.EventHandler(this.showCouples_Click);
+            // 
+            // rectangleMethodButton
+            // 
+            this.rectangleMethodButton.Font = new System.Drawing.Font("Rubik", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rectangleMethodButton.Location = new System.Drawing.Point(518, 237);
+            this.rectangleMethodButton.Name = "rectangleMethodButton";
+            this.rectangleMethodButton.Size = new System.Drawing.Size(167, 69);
+            this.rectangleMethodButton.TabIndex = 12;
+            this.rectangleMethodButton.Text = "Метод прямоугольников";
+            this.rectangleMethodButton.UseVisualStyleBackColor = true;
+            this.rectangleMethodButton.Click += new System.EventHandler(this.rectangleMethodButton_Click);
+            // 
+            // drawFuncButton
+            // 
+            this.drawFuncButton.Font = new System.Drawing.Font("Rubik", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.drawFuncButton.Location = new System.Drawing.Point(692, 312);
+            this.drawFuncButton.Name = "drawFuncButton";
+            this.drawFuncButton.Size = new System.Drawing.Size(132, 80);
+            this.drawFuncButton.TabIndex = 13;
+            this.drawFuncButton.Text = "Построить функцию";
+            this.drawFuncButton.UseVisualStyleBackColor = true;
+            this.drawFuncButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(737, 173);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(34, 20);
+            this.textBoxA.TabIndex = 14;
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(737, 199);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(34, 20);
+            this.textBoxB.TabIndex = 15;
+            // 
+            // textBoxN
+            // 
+            this.textBoxN.Location = new System.Drawing.Point(737, 225);
+            this.textBoxN.Name = "textBoxN";
+            this.textBoxN.Size = new System.Drawing.Size(34, 20);
+            this.textBoxN.TabIndex = 16;
+            // 
+            // alable
+            // 
+            this.alable.AutoSize = true;
+            this.alable.Location = new System.Drawing.Point(715, 176);
+            this.alable.Name = "alable";
+            this.alable.Size = new System.Drawing.Size(16, 13);
+            this.alable.TabIndex = 17;
+            this.alable.Text = "a:";
+            // 
+            // blable
+            // 
+            this.blable.AutoSize = true;
+            this.blable.Location = new System.Drawing.Point(715, 202);
+            this.blable.Name = "blable";
+            this.blable.Size = new System.Drawing.Size(16, 13);
+            this.blable.TabIndex = 18;
+            this.blable.Text = "b:";
+            // 
+            // Nlabel
+            // 
+            this.Nlabel.AutoSize = true;
+            this.Nlabel.Location = new System.Drawing.Point(715, 228);
+            this.Nlabel.Name = "Nlabel";
+            this.Nlabel.Size = new System.Drawing.Size(18, 13);
+            this.Nlabel.TabIndex = 19;
+            this.Nlabel.Text = "N:";
+            // 
+            // integralButton
+            // 
+            this.integralButton.Font = new System.Drawing.Font("Rubik", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.integralButton.Location = new System.Drawing.Point(692, 253);
+            this.integralButton.Name = "integralButton";
+            this.integralButton.Size = new System.Drawing.Size(132, 53);
+            this.integralButton.TabIndex = 20;
+            this.integralButton.Text = "Метод трапеций";
+            this.integralButton.UseVisualStyleBackColor = true;
+            this.integralButton.Click += new System.EventHandler(this.integralButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 523);
+            this.Controls.Add(this.integralButton);
+            this.Controls.Add(this.Nlabel);
+            this.Controls.Add(this.blable);
+            this.Controls.Add(this.alable);
+            this.Controls.Add(this.textBoxN);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.drawFuncButton);
+            this.Controls.Add(this.rectangleMethodButton);
+            this.Controls.Add(this.countButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.shouDotsButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.showCouples);
+            this.Controls.Add(this.importButton);
+            this.Name = "Form1";
+            this.Text = "Приближение функций";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button importButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button shouDotsButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button countButton;
+        private System.Windows.Forms.Button showCouples;
+        private System.Windows.Forms.Button rectangleMethodButton;
+        private System.Windows.Forms.Button drawFuncButton;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxN;
+        private System.Windows.Forms.Label alable;
+        private System.Windows.Forms.Label blable;
+        private System.Windows.Forms.Label Nlabel;
+        private System.Windows.Forms.Button integralButton;
+    }
+}
+
---
+++ 4_semestr/VichMath/Lab5/Lab4/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Globalization;
+using System.Windows.Forms.VisualStyles;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void RefreshForm()
+        {
+            panel1.Invalidate();
+        }
+
+        void ClearForm()
+        {
+            Main.dots.Clear();
+            Main.lines.Clear();
+            Main.curves.Clear();
+            Main.couples = new double[1, 1];
+            Main.numOfCouples = 0;
+            Graphic.scale = 100;
+            label1.Text = "Импортируйте пары значений";
+            RefreshForm();
+        }
+
+        double RectangleMethod(double a, double b, double h)
+        {
+            double result = 0;
+
+            double N = (b - a) / h;
+
+            for(double i = 0; i < N; i++)
+            {
+                result += h * Main.CountFunc(a + (i + 0.5) * h);
+            }
+
+            return result;
+        }
+
+        double TrapMethod(double a, double b, double h)
+        {
+            double result = 0;
+
+            double N = (b - a) / h;
+
+            double X1, X2 = a;
+
+            for (double i = 1; i <= N; i++)
+            {
+                X1 = X2;
+                X2 = a + i * h;
+                result += h * (Main.CountFunc(X1) + Main.CountFunc(X2)) / 2;
+            }
+
+            return result;
+        }
+
+        private void importButton_Click(object sender, EventArgs e)
+        {
+            string fileText = "";
+            Form2 importForm = new Form2();
+            importForm.Owner = this;
+            importForm.ShowDialog();
+            if (importForm.DialogResult == DialogResult.OK)
+            {
+                StreamReader file;
+                try
+                {
+                    file = new StreamReader(Main.sourcePath);
+                    fileText = file.ReadToEnd();
+                }
+                catch
+                {
+                    MessageBox.Show("Введён неверный путь к файлу!");
+                    return;
+                }
+                file.Close();
+
+                Main.sourcePath = "";
+
+                CultureInfo temp_culture = Thread.CurrentThread.CurrentCulture;
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-US");
+
+                try
+                {
+                    ClearForm();
+                    string[] separators = {"\n", " ", "\t"};
+                    string[] splittedText = fileText.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                    //string text = "";
+
+                    Main.numOfCouples = int.Parse(splittedText[0]);
+                    Main.couples = new double[Main.numOfCouples, 2];
+
+                    for (int i = 0; i < Main.numOfCouples; i++)
+                    {
+                        for (int j = 0; j < 2; j++)
+                        {
+                            //MessageBox.Show("+" + splittedText[1 + i * 2 + j] + "+");
+                            Main.couples[i, j] = double.Parse(splittedText[1 + i * 2 + j]);
+                            //text += Main.couples[i, j].ToString() + " ";
+                        }
+                        //text += '\n';
+                    }
+
+                    //MessageBox.Show(text);
+                }
+                catch
+                {
+                    MessageBox.Show("Файл не прочитан");
+                }
+                Thread.CurrentThread.CurrentCulture = temp_culture;
+                RefreshForm();
+                Graphic.GetDelta();
+                Graphic.ImportCouples();
+            }
+        }
+
+        private void showCouples_Click(object sender, EventArgs e)
+        {
+            string text = Main.numOfCouples.ToString() + " пар:\n";
+            for (int i = 0; i < Main.numOfCouples; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    text += Main.couples[i, j].ToString() + " ";
+                }
+                text += '\n';
+            }
+            MessageBox.Show(text);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = panel1.CreateGraphics();
+            Pen pen = new Pen(Color.Black, 1);
+
+            graphics.DrawLine(pen, new Point(0, 250), new Point(500, 250));
+            graphics.DrawLine(pen, new Point(250, 0), new Point(250, 500));
+            int x = 250;
+            int y = 250;
+            while(x > 0)
+            {
+                x -= Graphic.scale;
+                graphics.DrawLine(pen, new Point(x, 240), new Point(x, 260));
+            }
+            x = 250;
+            while (x < 500)
+            {
+                x += Graphic.scale;
+                graphics.DrawLine(pen, new Point(x, 240), new Point(x, 260));
+            }
+            while (y > 0)
+            {
+                y -= Graphic.scale;
+                graphics.DrawLine(pen, new Point(240, y), new Point(260, y));
+            }
+            y = 250;
+            while (y < 500)
+            {
+                y += Graphic.scale;
+                graphics.DrawLine(pen, new Point(240, y), new Point(260, y));
+            }
+
+            foreach (Line i in Main.lines) // Отрисовка линий
+            {
+                graphics.DrawLine(new Pen(Color.Red, 2), new Point(i.firstPoint.x, i.firstPoint.y), new Point(i.secondPoint.x, i.secondPoint.y));
+            }
+
+            foreach (Dot i in Main.dots) // Отрисовка точек
+            {
+                graphics.DrawRectangle(new Pen(Color.Green, 2), i.x - 1, i.y - 1, 3, 3);
+            }
+
+            foreach (Curve i in Main.curves) // Отрисовка кривых
+            {
+                graphics.DrawCurve(new Pen(Color.Red, 1), i.ConvertToPoints());
+            }
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            RefreshForm();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            ClearForm();
+        }
+
+        private void shouDotsButton_Click(object sender, EventArgs e)
+        {
+            string result = "";
+            int i = 1;
+            foreach(Dot x in Main.dots)
+            {
+                result += i.ToString() + ": " + x.x.ToString() + "/" + x.y.ToString() + "\n";
+                i++;
+            }
+            MessageBox.Show(result);
+        }
+
+        private void countButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                label1.Text = Main.CountFunc(double.Parse(textBox1.Text)).ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Введено неверное число!");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rectangleMethodButton_Click(object sender, EventArgs e)
+        {
+            double a, b, h;
+            try
+            {
+                a = double.Parse(textBoxA.Text);
+                b = double.Parse(textBoxB.Text);
+                h = double.Parse(textBoxN.Text);
+                label1.Text = "S: " + RectangleMethod(a, b, h).ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Введены неверные значения!");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Main.couples = new double[9, 2];
+            Main.numOfCouples = 9;
+            int k = 0;
+            for(double i = -1.5; i < 3; i += 0.5)
+            {
+                Main.couples[k, 0] = i;
+                Main.couples[k, 1] = Main.CountFunc(i);
+                k++;
+            }
+
+            Graphic.GetDelta();
+            Graphic.ImportCouples();
+
+            Curve curve = new Curve();
+            foreach(Dot i in Main.dots)
+            {
+                curve.dots.Add(i);
+            }
+            Main.curves.Add(curve);
+            RefreshForm();
+        }
+
+        private void integralButton_Click(object sender, EventArgs e)
+        {
+            double a, b, h;
+            try
+            {
+                a = double.Parse(textBoxA.Text);
+                b = double.Parse(textBoxB.Text);
+                h = double.Parse(textBoxN.Text);
+                label1.Text = "S: " + TrapMethod(a, b, h).ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Введены неверные значения!");
+            }
+        }
+    }
+}
