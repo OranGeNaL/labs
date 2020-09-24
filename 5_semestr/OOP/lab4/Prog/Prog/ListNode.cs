9b3d6f91184e2d01ba53fe0810818a06f3344c1f
OranGeNaL <pshenichniy2514@gmail.com>
2020-09-24 19:00:00 +1000

    Лаба

---
+++ 5_semestr/OOP/lab4/Prog/Prog/ListNode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prog
+{
+    class ListNode<T>
+    {
+        ListContainer<T> container;
+
+        public ListNode<T> prev;
+        public ListNode<T> next;
+
+        public T Value { get; set; }
+
+        public ListNode(T _value, ListContainer<T> _container)
+        {
+            Value = _value;
+            container = _container;
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
