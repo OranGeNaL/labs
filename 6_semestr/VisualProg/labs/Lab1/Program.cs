b3c8cf4691d677e6aa34933576ed617180e83291
OranGeNaL <39371893+OranGeNaL@users.noreply.github.com>
2021-02-16 21:15:37 +1000

    Сделал первую лабу и практику по ВП

---
+++ 6_semestr/VisualProg/labs/Lab1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FirstTask();
+            //SecondTask();
+            ThirdTask();
+        }
+
+        static void FirstTask()
+        {
+            float A = float.Parse(Console.ReadLine());
+            int I = int.Parse(Console.ReadLine());
+            double C = double.Parse(Console.ReadLine());
+            bool L = Convert.ToBoolean(Console.ReadLine());
+            string N = Console.ReadLine();
+
+            Console.WriteLine("A: {0,7:F5}\n", A);
+            Console.WriteLine("I: {0,5:D5}\n", I);
+            Console.WriteLine("C: {0,7:F5}\n", C);
+            Console.WriteLine("L: {0}\n", L);
+            Console.WriteLine("N: {0}\n", N);
+        }
+
+        static void SecondTask()
+        {
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            double z = double.Parse(Console.ReadLine());
+
+            double znam = x * x + Math.Max(z * z * z, x * y);
+            if (znam == 0)
+            {
+                Console.WriteLine("0 в знаменателе!!!");
+                return;
+            }
+
+            Console.WriteLine("Результат: {0}", Math.Min(x * x, y + z) / znam);
+        }
+
+        static void ThirdTask()
+        {
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+
+            if(Math.Sqrt(x*x + y*y) > 12)
+            {
+                Console.WriteLine("Точка входит в область 4.");
+                return;
+            }
+
+            if(x*y >= 0)
+            {
+                Console.WriteLine("Точка входит в область 2.");
+                return;
+            }
+
+            if(x < 0)
+            {
+                Console.WriteLine("Точка входит в область 1.");
+                return;
+            }
+
+            Console.WriteLine("Точка входит в область 3.");
+        }
+    }
+}
---
+++ 6_semestr/VisualProg/labs/ValyaLab1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace ValyaLab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //First();
+            //Second();
+            Third();
+        }
+
+        static void First()
+        {
+            Console.Write("Введите переменную A(int): ");
+            int A = int.Parse(Console.ReadLine());
+            Console.Write("Введите переменную I(int): ");
+            int I = int.Parse(Console.ReadLine());
+            Console.Write("Введите переменную C(double): ");
+            double C = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите переменную L(bool): ");
+            bool L = bool.Parse(Console.ReadLine());
+            Console.Write("Введите переменную N(string): ");
+            string N = Console.ReadLine();
+
+            Console.Write($"\nA = {A}\nI = {I}\nC = {C}\nL = {L}\nN = {N}\n");
+        }
+
+        static void Second()
+        {
+            Console.Write("x: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("y: ");
+            double y = double.Parse(Console.ReadLine());
+            Console.Write("z: ");
+            double z = double.Parse(Console.ReadLine());
+
+            try
+            {
+                double znam = x * x * x + z * z;
+                if(znam == 0)
+                    throw new DivideByZeroException();
+
+                Console.WriteLine("F = {0}", Math.Pow(Math.Min(x, Math.Max(x + z, y)), 2)/znam);
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void Third()
+        {
+            Console.Write("x: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("y: ");
+            double y = double.Parse(Console.ReadLine());
+
+            int N = 0;
+            double r = Math.Sqrt(x * x + y * y);
+            double phi = Math.Acos(x / r);
+
+            if(r > 12)
+            {
+                N = 4;
+            } else if ((phi >= 0 && phi <= Math.PI/2) || (phi >= Math.PI && phi <= Math.PI*3.0/2) )
+            {
+                if (y > 0)
+                    N = 2;
+                else
+                    N = 3;
+            }
+            else if (phi > Math.PI/2 && phi < Math.PI)
+            {
+                if (y < 0)
+                    N = 2;
+                else
+                    N = 1;
+            }
+            else
+            {
+                N = 3;
+            }
+
+            Console.WriteLine($"Точка принадлежит области под номером {N}");
+        }
+    }
+}
