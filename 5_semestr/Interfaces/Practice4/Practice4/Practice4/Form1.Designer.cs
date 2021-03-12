dd3d2dbc9faa1a29950e823fc31d8fd778134a3e
OranGeNaL <39371893+OranGeNaL@users.noreply.github.com>
2021-03-12 22:38:33 +1000

    Завтра визуалку буду делать

--- 5_semestr/Interfaces/Practice4/Practice4/Practice4/Form1.Designer.cs
+++ 5_semestr/Interfaces/Practice4/Practice4/Practice4/Form1.Designer.cs
@@ -24,8 +24,8 @@ namespace Practice4
         #region Код, автоматически созданный конструктором форм Windows
 
         /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
@@ -95,7 +95,7 @@ namespace Practice4
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
             this.label1.Location = new System.Drawing.Point(362, 84);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(53, 73);
@@ -105,7 +105,7 @@ namespace Practice4
             // 
             // label2
             // 
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
             this.label2.Location = new System.Drawing.Point(361, 476);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(75, 39);
@@ -130,7 +130,6 @@ namespace Practice4
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
 
         #endregion
